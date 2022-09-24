1c27ac9a9b7800e60bc9b2363ac6c49537212358
tomimarkus991 <tomimarkusalber@gmail.com>
2022-09-24 17:19:14 +0300

    Publish

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,10 @@
-import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const packageJson = require("./package.json");
 
 export default [
@@ -24,6 +26,16 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss({
+        config: {
+          path: "./postcss.config.js",
+        },
+        extensions: [".css"],
+        minimize: true,
+        inject: {
+          insertAt: "top",
+        },
+      }),
     ],
   },
   {
